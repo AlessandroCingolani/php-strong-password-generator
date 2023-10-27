c591bd567074e00c2067da4e9c7e02b34f0e7cb2
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-27 16:14:44 +0200

    [ add ] css and change class ecc

--- index.php
+++ index.php
@@ -26,26 +26,30 @@ if(isset($_POST['password_length'])){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- BOOTSTRAP -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- CSS -->
+  <link rel="stylesheet" href="./css/style.css">
   <title>PHP Strong Password Generator</title>
 </head>
 <body>
-  <div class="container">
-    <div class="password-generator text-center ">
-      <h1>Strong Password Generator</h1>
-      <h2>Genera una password sicura</h2>
-      <div class="message">
-        <p><?php echo $instructions ?></p>
+  <div class="container d-flex justify-content-center">
+    <div class="password-generator ">
+      <div class="title text-center ">
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+      </div>
+      <div class="message d-flex align-items-center ">
+        <span><?php echo $instructions ?></span>
       </div>
       <div class="form">
-        <form action="index.php" method="POST">
-          <div class="row">
+        <form class="h-100" action="index.php" method="POST">
+          <div class="row h-100 align-items-center p-3">
             <div class="col">
               <h5>Lunghezza password:</h5>
               <button type="submit" class="btn btn-primary">Invia</button>
-              <button class="btn btn-secondary">Annulla</button>
+              <button type="reset" class="btn btn-secondary">Annulla</button>
             </div>
-            <div class="col">
-              <input type="number" placeholder="Number" name="password_length" id="password_length">
+            <div class="col align-self-start ">
+              <input  type="number" placeholder="Number" name="password_length" id="password_length">
             </div>
           </div>
         </form>
