0b242d11cc3c640b79fe8341b4680ea22d205640
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-27 15:15:09 +0200

    [ add ] function passed length gen n rand char

--- partials/functions.php
+++ partials/functions.php
@@ -1,2 +1,14 @@
 <?php
 
+function random_generator($length){
+
+  $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  $random_password = '';
+
+  for($i = 0; $i < $length; $i++){
+    $random_password.= $chars[rand(0, strlen($chars) - 1)];
+  }
+
+  return $random_password;
+}
+
