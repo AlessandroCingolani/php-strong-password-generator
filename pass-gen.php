f99294889aad24bb878bd6fd39074485bf5c4c54
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-27 16:15:09 +0200

    [ add ] css and del var dump

--- pass-gen.php
+++ pass-gen.php
@@ -5,7 +5,6 @@ session_start();
 if(isset($_SESSION["number_to_generate"])){
   $number_input = $_SESSION["number_to_generate"];
   $pass_generated = random_generator($number_input);
-  var_dump($pass_generated);
 }else {
   header("Location: ./index.php");
 }
@@ -20,14 +19,16 @@ if(isset($_SESSION["number_to_generate"])){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- BOOTSTRAP -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <title>PHP Strong Password Generator</title>
+  <!-- CSS -->
+  <link rel="stylesheet" href="./css/style.css">
+  <title>Password Generated</title>
 </head>
 <body>
   <div class="container">
     <div class="password-generator text-center ">
-      <h2>Genera una password sicura</h2>
+      <h2>La password generata è:</h2>
       <div class="message">
-        <p><?php echo $pass_generated ?></p>
+        <span><?php echo $pass_generated ?></span>
       </div>
       <a href="index.php"><button class="btn btn-primary ">Torna alla homepage</button></a>
     </div>
